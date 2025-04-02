f4ab1107dbfcef4645547a4ca4d7610951f3e153
natashanyokabi <natashawairimu27@gmail.com>
2025-04-03 01:22:42 +0300

    dom

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('p').textContent = ('This is really cool!');
+  
+    });
+    
+    console.log(
+      "This is really cool!"
+    );
